9904117e380fef49ff9f803a32e263abefd7621c
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-11-11 06:46:57 +0200

    build: update dist/utils/helpers.d.ts

--- dist/utils/helpers.d.ts
+++ dist/utils/helpers.d.ts
@@ -1,5 +1,7 @@
 import { Book } from '../database/models';
-export declare const formatBookCaption: (book: Book) => Promise<string>;
+export declare const formatBookCaption: (book: Book, tags?: Array<{
+    name: string;
+}>) => Promise<string>;
 export declare const formatRequestInfo: (request: any, book: Book) => string;
 export declare const escapeHtml: (text: string) => string;
 export declare function showLoadingAnimation(ctx: any, message: string): Promise<number>;
