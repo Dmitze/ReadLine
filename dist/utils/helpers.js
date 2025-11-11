5129b03ab37b45bd2ce8756bedcdc108d45ce6a0
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-11-11 06:46:57 +0200

    build: update dist/utils/helpers.js

--- dist/utils/helpers.js
+++ dist/utils/helpers.js
@@ -1,23 +1,62 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.escapeHtml = exports.formatRequestInfo = exports.formatBookCaption = void 0;
 exports.showLoadingAnimation = showLoadingAnimation;
 exports.updateLoadingMessage = updateLoadingMessage;
 exports.createProgressBar = createProgressBar;
 exports.formatStepProgress = formatStepProgress;
-const tagFunctions_1 = require("../database/tagFunctions");
-const formatBookCaption = async (book) => {
+const formatBookCaption = async (book, tags) => {
     let caption = `╔═══════════════════════╗\n`;
     caption += `📖 *${book.title}*\n`;
     caption += `╚═══════════════════════╝\n\n`;
     caption += `✍️ *Автор:* ${book.author}\n`;
     const genreEmoji = getGenreEmoji(book.genre);
     caption += `${genreEmoji} *Жанр:* ${book.genre}\n`;
-    if (book.id) {
+    if (tags) {
+        if (tags.length > 0) {
+            const tagNames = tags.map(t => `#${t.name.replace(/\s+/g, '_')}`).join(' ');
+            caption += `🏷️ *Теги:* ${tagNames}\n`;
+        }
+    }
+    else if (book.id) {
         try {
-            const tags = await (0, tagFunctions_1.getBookTags)(book.id);
-            if (tags.length > 0) {
-                const tagNames = tags.map(t => `#${t.name.replace(/\s+/g, '_')}`).join(' ');
+            const { getBookTags } = await Promise.resolve().then(() => __importStar(require('../database/tagFunctions')));
+            const loadedTags = await getBookTags(book.id);
+            if (loadedTags.length > 0) {
+                const tagNames = loadedTags.map(t => `#${t.name.replace(/\s+/g, '_')}`).join(' ');
                 caption += `🏷️ *Теги:* ${tagNames}\n`;
             }
         }
