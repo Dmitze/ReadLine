303238bf06cb5049117591cff14a962cd9ba4134
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-11-14 16:22:39 +0200

    build: update dist/scenes/addBookScene.d.ts

--- dist/scenes/addBookScene.d.ts
+++ dist/scenes/addBookScene.d.ts
@@ -1,8 +1,5 @@
 import { Scenes } from 'telegraf';
 import { BotContext } from '../types/telegraf';
-declare const addBookScene: Scenes.WizardScene<import("telegraf").Context<import("@telegraf/types").Update> & {
-    scene: Scenes.SceneContextScene<BotContext, Scenes.WizardSessionData>;
-    wizard: Scenes.WizardContextWizard<BotContext>;
-}>;
+declare const addBookScene: Scenes.WizardScene<BotContext>;
 export default addBookScene;
 //# sourceMappingURL=addBookScene.d.ts.map
