35a483175c82b6a41d08b7845d904f2626bee437
Дмитрий Шивачов <102044731+Dmitze@users.noreply.github.com>
2025-11-25 09:47:14 +0200

    build: update dist/database/models.d.ts

--- dist/database/models.d.ts
+++ dist/database/models.d.ts
@@ -1,4 +1,5 @@
 export * from './tables';
+export { getBooksByIds } from './tables/books';
 import { db as database } from './tables/db';
 export { database as db };
 //# sourceMappingURL=models.d.ts.map
